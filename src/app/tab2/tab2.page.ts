eb9bd04519398c22a409b8189889e5696ecd8d69
QuentinGeerts <quentin.geerts@bstorm.be>
2024-03-27 22:41:32 +0100

    .

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,74 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {Component} from '@angular/core';
+import {
+  IonButton,
+  IonCheckbox,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonList,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import {FormsModule} from "@angular/forms";
+import {NgClass} from "@angular/common";
+import {addIcons} from "ionicons";
+import {trashOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem, IonText,
+    IonButton, FormsModule, IonCheckbox, NgClass, IonIcon, IonRow, IonGrid, IonCol]
 })
 export class Tab2Page {
+  userInput: string = "";
+  todos: Todo[] = [
+    {label: "Coder l'application Angular/Ionic", isDone: true},
+    {label: "Faire la vaisselle", isDone: false},
+    {label: "Nettoyer la litière", isDone: true},
+    {label: "Ranger les linges", isDone: false},
+  ];
 
-  constructor() {}
+  constructor() {
+    addIcons({trashOutline})
+  }
 
+  addTask(): void {
+
+    // Nettoyage de la donnée
+    // : Suppression des espaces avant et après
+    let input: string = this.userInput.trim();
+
+    // Si l'utilisateur n'a rien rentré, on annule
+    if (input === '') return;
+
+    // Si le label est déjà dans la liste, on annule
+    if (this.todos.find((task: Todo) => task.label.toLowerCase() === input.toLowerCase())) return;
+
+    this.todos.push({label: input, isDone: false});
+    this.userInput = "";
+
+
+  }
+
+  deleteTask(index: number): void {
+    this.todos.splice(index, 1);
+  }
+
+  clearTodos() {
+    this.todos.length = 0;
+  }
+}
+
+export interface Todo {
+  label: string;
+  isDone: boolean;
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,63 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  ToastController,
+  IonItem,
+  IonInput, IonButton, IonCardHeader, IonCard, IonCardSubtitle, IonRow, IonImg, IonText, IonCardTitle, IonCardContent
+} from '@ionic/angular/standalone';
+import {DatePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonCardHeader, IonCard, IonCardSubtitle, IonRow, IonImg, IonText, IonCardTitle, IonCardContent, DatePipe, FormsModule],
 })
 export class Tab3Page {
-  constructor() {}
+  API_GEO_URL: string = 'http://api.openweathermap.org/geo/1.0/direct?q=__city__&appid=05f9485642a4d913df1e2b5fd6079d96';
+  API_WEATHER_URL: string = 'https://api.openweathermap.org/data/2.5/weather?lat=__lat__&lon=__lon__&units=metric&lang=fr&appid=05f9485642a4d913df1e2b5fd6079d96'
+  cityName: string = '';
+
+  weatherDataIcon: string = '';
+
+  geolocalisation: any = null;
+  weatherData: any = null;
+
+  constructor(private toaster: ToastController) { }
+
+  async search() {
+    this.geolocalisation = await fetch(this.API_GEO_URL.replace('__city__', this.cityName))
+      .then(response => response.json());
+
+    if (this.geolocalisation.length === 0) {
+      await this.presentToast();
+    } else {
+      this.weatherData = await fetch(this.API_WEATHER_URL
+        .replace('__lat__', this.geolocalisation[0].lat)
+        .replace('__lon__', this.geolocalisation[0].lon))
+        .then(response => response.json());
+      this.weatherDataIcon = `https://openweathermap.org/img/wn/${this.weatherData.weather[0].icon}.png`;
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toaster.create({
+      message: 'No weather data found',
+      duration: 3000,
+      color: "danger",
+      buttons: [
+        {
+          role: 'cancel',
+          icon: 'close'
+        }
+      ]
+
+    })
+    await toast.present();
+  }
 }
